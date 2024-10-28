d270435243460770765a66132250a9c3009756ba
Gabriel Cozma <me@gabs.eu.org>
2024-10-28 18:11:11 +0200

    fix: vite config imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from "npm:vite";
-import { svelte } from "npm:@sveltejs/vite-plugin-svelte";
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
